0da14ffe216819b8623dcafaf47ba7b087af005d
Alma <alma9000@gmail.com>
2020-03-29 23:38:59 +0200

    Made login work
    - Removed dysfunctional imports in application.py and helpers.py
    - In application.py:
    	- Added sqlite3 library to work with finance.db
    	- Rewrote connection and retrieve user functions
    	- Rewrote queries in login function to compare username and password
    	  hash with finance.db
    - Added the use of a config file for getting API_KEY so
      I don't need to set it.
    - Added gitignore and replaced config file with example.
    - Pip installed virtualenv - thus the venv folder.

--- application.py
+++ application.py
@@ -1,16 +1,25 @@
+#!/bin/env python
 import os
-
-from cs50 import SQL
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
-from flask_session import Session
+import sqlite3
+#from flask import Session
 from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+
+#from cs50 import SQL
+from flask import (Flask, flash, jsonify, redirect, render_template, request,
+				   session)
+from werkzeug.exceptions import (HTTPException, InternalServerError,
+								 default_exceptions)
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
 
+
 # Configure application
 app = Flask(__name__)
+
+#trying to use config file for setting keys
+app.config.from_pyfile("config.py")
+
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -18,124 +27,170 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Ensure responses aren't cached
 @app.after_request
 def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
+	response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+	response.headers["Expires"] = 0
+	response.headers["Pragma"] = "no-cache"
+	return response
 
 # Custom filter
-app.jinja_env.filters["usd"] = usd
+#app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+#Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+# set db with sqlite3 instead of cs50's SQL ("Configure CS50 Library to use SQLite database")
+def retrieveUsers():
+	con = sqlite3.connect("finance.db")
+	cur = con.cursor()
+	cur.execute("SELECT username, hash FROM users")
+	users = cur.fetchall()
+	con.close()
+	return users
 
+def retrieveUser(username):
+	con = sqlite3.connect("finance.db")
+	cur = con.cursor()
+	cur.execute('SELECT username, hash, id FROM users WHERE username=?', (username,))
+	users = cur.fetchall()
+	con.close()
+
+	if not users:
+		print("no such user")
+		return "user does not exist"
+	else:
+		return users[0]
+
+
+
+apikey = app.config["API_KEY"]
+os.environ["API_KEY"] = apikey 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
-    raise RuntimeError("API_KEY not set")
+	raise RuntimeError("API_KEY not set")
 
 
 @app.route("/")
 @login_required
 def index():
-    """Show portfolio of stocks"""
-    return apology("TODO")
+	"""Show portfolio of stocks"""
+	return apology("TODO")
 
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
-    """Buy shares of stock"""
-    return apology("TODO")
+	"""Buy shares of stock"""
+	return apology("TODO")
 
 
 @app.route("/history")
 @login_required
 def history():
-    """Show history of transactions"""
-    return apology("TODO")
+	"""Show history of transactions"""
+	return apology("TODO")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user in"""
+	"""Log user in"""
 
-    # Forget any user_id
-    session.clear()
+	# Forget any user_id
+	session.clear()
 
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
+	# User reached route via POST (as by submitting a form via POST)
+	if request.method == "POST":
 
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
+		# Ensure username was submitted
+		if not request.form.get("username"):
+			return apology("must provide username", 403)
 
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
+		# Ensure password was submitted
+		elif not request.form.get("password"):
+			return apology("must provide password", 403)
 
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username"))
+		# Query database for username
+		username = request.form.get("username")
+		password = request.form.get("password")
 
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+		currentUser = retrieveUser(username)
+		currentUsername = currentUser[0]
+		currentHash = currentUser[1]
+		currentId = currentUser[2]
 
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+		# Ensure username exists and password is correct
+		if currentUsername == username:
+			# Username exists, check password hash:
+			if check_password_hash(currentHash, password):
+				# Hash was correct - log user in
+				print("User can log in")
+				session["user_id"] = currentId
+			else:
+				# User exists but password is incorrect
+				print("Incorrect password")
+				return apology("invalid password", 403)
+		else:
+			# User does not exist
+			print("user doesn't exist")
+			return apology("invalid username", 403)
+		
+		"""
+		CS50's way of doing it with CS50's SQL library:
+		if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+			return apology("invalid username and/or password", 403)
 
-        # Redirect user to home page
-        return redirect("/")
+		# Remember which user has logged in
+		#session["user_id"] = rows[0]["id"]
+		"""
 
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
+		# Redirect user to home page
+		return redirect("/")
+
+	# User reached route via GET (as by clicking a link or via redirect)
+	else:
+		return render_template("login.html")
 
 
 @app.route("/logout")
 def logout():
-    """Log user out"""
+	"""Log user out"""
 
-    # Forget any user_id
-    session.clear()
+	# Forget any user_id
+	session.clear()
 
-    # Redirect user to login form
-    return redirect("/")
+	# Redirect user to login form
+	return redirect("/")
 
 
 @app.route("/quote", methods=["GET", "POST"])
 @login_required
 def quote():
-    """Get stock quote."""
-    return apology("TODO")
+	"""Get stock quote."""
+	return apology("TODO")
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user"""
-    return apology("TODO")
+	"""Register user"""
+	return apology("TODO")
 
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
 def sell():
-    """Sell shares of stock"""
-    return apology("TODO")
+	"""Sell shares of stock"""
+	return apology("TODO")
 
 
 def errorhandler(e):
-    """Handle error"""
-    if not isinstance(e, HTTPException):
-        e = InternalServerError()
-    return apology(e.name, e.code)
+	"""Handle error"""
+	if not isinstance(e, HTTPException):
+		e = InternalServerError()
+	return apology(e.name, e.code)
 
 
 # Listen for errors
 for code in default_exceptions:
-    app.errorhandler(code)(errorhandler)
+	app.errorhandler(code)(errorhandler)
---
+++ example.config.py
@@ -0,0 +1,7 @@
+"""
+Stuff needs to be filled out correctly. This is an example file. The app uses config.py in this directory.
+"""
+
+DEBUG=1
+SECRET_KEY="anothersecretkey"
+API_KEY="get your own API key :-D :-D"
