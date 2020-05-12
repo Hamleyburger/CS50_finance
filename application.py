bfaca29e939e17f30528a45338958e995968e914
Alma <alma9000@gmail.com>
2020-05-12 13:47:19 +0200

    added master comment in application.py!

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 """
 This is the master version of CS50 Finance. This does not depend on CS50 IDE
 This comment is added to master after CS50 IDE branch was made
+No This comment was.
 """
 
 from application import app
