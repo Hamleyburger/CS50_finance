8fe316bfd0cf489d57e292240fc0c626de4df938
Alma <alma9000@gmail.com>
2020-05-17 19:08:57 +0200

    Fixed and cleaned client side validation
    - in JS validation functions I changed all values to be acquired from html elements rather than wtforms' form values since these depend on server requests which is exactly what I am blocking with client side validation.
    - Added comments to and tidied up JS functions
    - moved JS functions to external script file
    - linked to script file with url_for "static" in the end of base template's body.
    - renamed JS validation functions to validateLogin and validateRegister

--- application/static/scripts.js
+++ application/static/scripts.js
@@ -1,46 +1,92 @@
+function validateRegister(form) {
 
-function validateForm(form) {
-
+    // Renaming vars for easier readability
+    usernameElement = document.getElementById("username");
+    passwordElement = document.getElementById("password");
+    confirmElement = document.getElementById("confirm_password");
+    username = usernameElement.value;
+    password = passwordElement.value;
+    password_confirm = confirmElement.value;
+    
+    // Reset invalid values before checking
+    usernameElement.classList.remove("is-invalid");
+    passwordElement.classList.remove("is-invalid");
+    confirmElement.classList.remove("is-invalid");
+    
+    // flag will be false if any check fails and onsubmit will return false
     var flag = true;
 
-    username = form.username.value;
-    if (!(username.length >= 4 && username.length <= 30)) {
-        var username = document.getElementById("username");
-        username.classList.add("is-invalid");
-        document.getElementById("username-feedback").innerHTML = "Username must be between 4 and 30 characters long"
-        flag = false;
-    }
-
-    pw = form.password.value;
-    if (!(pw.length > 7)) {
-        var password = document.getElementById("password");
-        password.classList.add("is-invalid");
-        document.getElementById("password-feedback").innerHTML = "Your password must be at least 8 characters long"
-        flag = false;
-    }
-
-    if (form.password.value !== form.confirm_password.value) {
-        var confirm_password = document.getElementById("confirm_password");
-        confirm_password.classList.add("is-invalid");
-        document.getElementById("confirm-feedback").innerHTML = "The passwords didn't match"
-        flag = false;
-    }
-
-    if (form.username.value == "") {
+    if (username == "") {
+        usernameElement.classList.add("is-invalid");
         document.getElementById("username-feedback").innerHTML = "You must provide a username";
-        username.classList.add("is-invalid");
+        flag = false;
+    }
+    else if (!(username.length >= 4 && username.length <= 30)) {
+        usernameElement.classList.add("is-invalid");
+        document.getElementById("username-feedback").innerHTML = "Username must be between 4 and 30 characters long";
         flag = false;
     }
 
-    if (form.password.value == "") {
+    if (password == "") {
+        passwordElement.classList.add("is-invalid");
         document.getElementById("password-feedback").innerHTML = "You must provide a password";
-        password.classList.add("is-invalid");
+        flag = false;
+    }
+    else if (!(password.length > 7)) {
+        passwordElement.classList.add("is-invalid");
+        document.getElementById("password-feedback").innerHTML = "Your password must be at least 8 characters long";
         flag = false;
     }
 
-    if (form.confirm_password.value == "") {
+    if (password_confirm == "") {
+        confirmElement.classList.add("is-invalid");
         document.getElementById("confirm-feedback").innerHTML = "You must provide a password";
-        confirm_password.classList.add("is-invalid");
+        flag = false;
+    }
+    else if (password !== password_confirm) {
+        confirmElement.classList.add("is-invalid");
+        document.getElementById("confirm-feedback").innerHTML = "The passwords didn't match";
+        flag = false;
+    }
+
+    return flag;
+}
+
+
+function validateLogin(form) {
+
+    // Renaming vars for easier readability
+    usernameElement = document.getElementById("username");
+    passwordElement = document.getElementById("password");
+    username = usernameElement.value;
+    password = passwordElement.value;
+
+    // Reset invalid values before checking
+    usernameElement.classList.remove("is-invalid");
+    passwordElement.classList.remove("is-invalid");
+
+    // flag will be false if any check fails and onsubmit will return false
+    var flag = true;
+
+    if (username == "") {
+        usernameElement.classList.add("is-invalid");
+        document.getElementById("username-feedback").innerHTML = "You must provide a username";
+        flag = false;
+    }
+    else if (!(username.length >= 4 && username.length <= 30)) {
+        usernameElement.classList.add("is-invalid");
+        document.getElementById("username-feedback").innerHTML = "Username must be between 4 and 30 characters long";
+        flag = false;
+    }
+
+    if (password == "") {
+        passwordElement.classList.add("is-invalid");
+        document.getElementById("password-feedback").innerHTML = "You must provide a password";
+        flag = false;
+    }
+    else if (!(password.length > 7)) {
+        passwordElement.classList.add("is-invalid");
+        document.getElementById("password-feedback").innerHTML = "Your password must be at least 8 characters long";
         flag = false;
     }
 
