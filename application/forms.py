20084e6cc5a4b98c84d9286e4474e697a50ca419
Alma <alma9000@gmail.com>
2020-06-19 17:35:47 +0200

    more validation, mostly JS
    - js form validation precedes server side validatil. JS is placed at bottom of each template in script tag.
    - JS validation for buy, sell and quote.
    - pressing buy or sell buttons now instantly attempts to include refreshed amount if this has been changed in input field. User must still press refresh first, though, for the price total to be updated.

--- application/forms.py
+++ application/forms.py
@@ -49,10 +49,13 @@ class LoginForm(FlaskForm):
 
 
 def validBuyAmount(form, field):
-    if form.shares_button.data:
-        # User refreshed amount. Refresh if > 0 and user can afford
+    if form.shares_button.data or form.buy_button.data:
+        # User refreshed amount either buy refresh or buy btn. Refresh if > 0 and user can afford
         amount = form.shares.data
+        print(f"amount {amount}")
         user = form.user
+        print(f"user {user}")
+        print(f"stock {form.stock}")
 
         canAfford = False
 
@@ -85,27 +88,30 @@ def validSymbol(form, field):
 
 
 def allowBuy(form, field):
-    if form.submit_button.data:
+    if form.buy_button.data:
         user = User.query.filter_by(id=session["user_id"]).first()
         stock = form.stock
-        try:
-            user.buy(stock.symbol, session["buystock"]["amount"])
-            flash(u"Purhased {} {}".format(
-                session["buystock"]["amount"], stock.name), "success")
-            print("User bought {} {}".format(session["buystock"]["amount"], stock.name))
-            session["buystock"] = {}
-            session["cash"] = user.cash
-        except Exception as e:
-            flash(f"{e}", "danger")
-            raise ValidationError(e)
+
+        # Only try selling if amount of shares is validated
+        if not form.shares.errors:
+            try:
+                user.buy(stock.symbol, session["buystock"]["amount"])
+                flash(u"Purhased {} {}".format(
+                    session["buystock"]["amount"], stock.name), "success")
+                print("User bought {} {}".format(session["buystock"]["amount"], stock.name))
+                session["buystock"] = {}
+                session["cash"] = user.cash
+            except Exception as e:
+                flash(f"{e}", "danger")
+                raise ValidationError(e)
 
 
 class BuyForm(FlaskForm):
     search = StringField("Search", id="symbolInput", render_kw={"placeholder": "Search for symbol"}, validators=[validSymbol])
-    search_button = SubmitField("Search", id="symbolBtn")
+    search_button = SubmitField("Search")
     shares = IntegerField("Shares", id="amountInput", default=1, validators=[validBuyAmount])
-    shares_button = SubmitField("Refresh", id="amountBtn")
-    submit_button = SubmitField("Buy", validators=[allowBuy])
+    shares_button = SubmitField("Refresh")
+    buy_button = SubmitField("Buy", validators=[allowBuy])
 
     def __init__(self, user, stock, *args, **kwargs):
         super(BuyForm, self).__init__(*args, **kwargs)
@@ -120,7 +126,6 @@ def validSellAmount(form, field):
         print("refresh btn pressed")
         # Refresh amount to sell
         amount = field.data
-        user = User.query.filter_by(id=session["user_id"]).first()
         stock = form.stock
         owned = stock.amount
 
@@ -154,7 +159,7 @@ class SellForm(FlaskForm):
 
     shares = IntegerField("Shares", default=1, validators=[validSellAmount])
     shares_button = SubmitField("Refresh")
-    submit_button = SubmitField("Sell", validators=[allowSell])
+    sell_button = SubmitField("Sell", validators=[allowSell])
 
     def __init__(self, user, stock, *args, **kwargs):
         super(SellForm, self).__init__(*args, **kwargs)
